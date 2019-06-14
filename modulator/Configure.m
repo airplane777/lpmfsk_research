0e0c6d60dbc2585bf8d0ab6b2ffadd1e7147339f
Matthew S Du <matthewdu@protonmail.com>
2019-06-14 10:46:41 +0100

    Randomise message, test pattern gen

--- modulator/Configure.m
+++ modulator/Configure.m
@@ -1,11 +1,11 @@
 % Prior knowledge
 
-%TX
+% TX
 FS        = 44100; % Sampling rate
 NCARRIERS = 8;     % Number of carriers
 TONE_SPC  = 2;     % Tone spacing factor
 
-%RX
+% RX
 FFT_SIZE  = 4410; % FFT window length
                   % Resolution of waterfall on frequency axis
 FFT_SHIFT = 300;  % FFT window shift step
--- modulator/Run_Modulate.m
+++ modulator/Run_Modulate.m
@@ -1,21 +1,25 @@
 % Initialise
 Configure;
 
-% Message to send
-msg = [1 1 1 1 1 1 1 1 8 1 8 1 8 1 8 1 2 3 6 7 8 3 1 4 1 5 8 2 6 5 3 5 8 8 7 8 3 2 3 8 4 6 2 6 4 3 3 8 3 2 7 8 1 8 1 8 1];
-
 % Modulation parameters
-bottom_freq     = 2000;
-baud_rate       = 10;
-amplitude       = 0.2;
+bottom_freq     = 5000;
+baud_rate       = 10;   % symbols per second
+amplitude       = 0.5;
+message_length  = 50;   % symbols
 
+% Generate random message
+msg = randi([1 NCARRIERS], 1, message_length);
 
+% Modulate message
 wav = Modulate(FS, NCARRIERS, baud_rate, bottom_freq, TONE_SPC, msg);
 wav = amplitude * wav;
 
-% Plot, play and save the result
-% spectrogram(wav, hamming(FFT_SIZE), FFT_OVERLAP, FFT_SIZE);
+% Plot spectrogram
 wf = Waterfall(FS, wav, FFT_SIZE, FFT_SHIFT);
 imagesc(wf)
+
+% Play and save audio
 sound(wav, FS);
-% audiowrite('modulated.wav', wav, FS);
+% id = 1;
+% filename = sprintf("%d_f%.2f_br%.2f_amp%.2f.wav", id, bottom_freq, baud_rate, amplitude);
+% audiowrite(filename, wav, FS);
