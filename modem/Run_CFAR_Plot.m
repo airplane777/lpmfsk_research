aa1bc974e8e53f5d038061c4530eb49c2abdfcb9
Matthew S Du <matthewdu@protonmail.com>
2019-06-26 17:03:32 +0100

    Add SNR peak search algorithm

--- modem/Run_CFAR_Plot.m
+++ modem/Run_CFAR_Plot.m
@@ -1,5 +1,37 @@
-figure;
+% Analyse data
+% Parameters
+peak_search_size = 200; % pixels
 
+figure;
+imagesc(wf);
+ntgt = 0;
+for i = peak_search_size + 1 : wf_size(1) - peak_search_size
+  for j = peak_search_size + 1 : wf_size(2) - peak_search_size
+    detected_flag = true;
+    for mask_i = i - peak_search_size : i + peak_search_size
+      for mask_j = j - peak_search_size : j + peak_search_size
+        if wf(mask_i, mask_j) > wf(i, j)
+          detected_flag = false;
+          break
+        end
+      end
+      if detected_flag == false
+        break
+      end
+    end
+    if detected_flag == true
+      ntgt = ntgt + 1;
+      rectangle('Position', [j - cell_size(2) / 2, i - cell_size(1) / 2, cell_size(2), cell_size(1)], 'EdgeColor', 'w')
+    end
+  end
+  w = waitbar(i / (wf_size(1) - peak_search_size));
+end
+
+close(w);
+
+% Plot data
+
+figure;
 subplot(2, 2, 1);
 imagesc(wf);
 title("Original Spectrogram");
