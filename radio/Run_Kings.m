b829b7d1223d172cf94d88621d111ecde06c6cf9
Matthew S Du <matthewdu@protonmail.com>
2019-08-06 10:13:33 +0100

    Plot critical distances as func of frequency

--- radio/Run_Kings.m
+++ radio/Run_Kings.m
@@ -1,19 +1,32 @@
 % Constants
 c          = 299792458;  % m/s, speed of light(radio)
 a          = 6371e3;     % m, Earth radius
-f          = 10e6;       % Hz, RF frequency
 epsilon_0  = 8.8542e-12; % Electric constant
 epsilon_rg = 80;         % Water + NaCl
 sigma_g    = 4;          % S/m, water + NaCl
+current    = 0.01;       % A, antenna current at centre
+he         = 0.1;        % m, antenna half length
 
-% Calculations
-lambda    = c / f;
-epsilon_c = epsilon_rg + ((1i * sigma_g) / (2 * pi * f * epsilon_0));
+FREQ_RANGE = 1e6 : 0.5e6 : 30e6; % Hz
+freq_range_size = size(FREQ_RANGE);
+dc = zeros(1, freq_range_size(2));
+di = zeros(1, freq_range_size(2));
 
-k0 = (2 * pi) / lambda; % In air
-kg = k0 * sqrt(abs(epsilon_c)); % On ground
+for i = 1 : freq_range_size(2)
+  f         = FREQ_RANGE(i);
+  lambda    = c / f;
+  epsilon_c = epsilon_rg + ((1i * sigma_g) / (2 * pi * f * epsilon_0));
 
+  k0 = (2 * pi) / lambda; % In air
+  kg = k0 * sqrt(abs(epsilon_c)); % On ground
 
-dc = a * (k0 * a / 2) ^ (-1 / 3);
-di = (2 * (kg ^ 2)) / (k0 ^ 3);
+  dc(i) = a * (k0 * a / 2) ^ (-1 / 3);
+  di(i) = (2 * (kg ^ 2)) / (k0 ^ 3);
+end
+
+figure;
+hold on;
+plot(FREQ_RANGE, dc);
+plot(FREQ_RANGE, di);
+
 
