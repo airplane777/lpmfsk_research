0cedc39c10d7df66b25d6cbe21f8387b3b5a7311
Matthew S Du <matthewdu@protonmail.com>
2019-08-06 23:27:50 +0100

    Add SNR calculation(with open issues), add worst case noise

--- radio/Run_Kings.m
+++ radio/Run_Kings.m
@@ -10,13 +10,21 @@ he         = 0.1;        % m, antenna half length
 
 FREQ_RANGE      = 4e6 : 1e6 : 30e6; % Hz
 freq_range_size = size(FREQ_RANGE);
-DIST_RANGE      = 100 : 50 : 10000;    % m
+DIST_RANGE      = 100 : 50 : 10000; % m
 dist_range_size = size(DIST_RANGE);
+BW              = 400;              % Hz
 
 % Malloc
-dc = zeros(1, freq_range_size(2));
-di = zeros(1, freq_range_size(2));
-b  = zeros(freq_range_size(2), dist_range_size(2));
+dc       = zeros(1, freq_range_size(2));
+di       = zeros(1, freq_range_size(2));
+b        = zeros(freq_range_size(2), dist_range_size(2));
+e        = zeros(freq_range_size(2), dist_range_size(2));
+snr      = zeros(freq_range_size(2), dist_range_size(2));
+db_noise = zeros(1, freq_range_size(2));
+db_signal = zeros(1, freq_range_size(2));
+
+% Run ITU data curve fitting
+Gen_Noise;
 
 for i = 1 : freq_range_size(2)
   % Phase 0, preparation
@@ -29,9 +37,11 @@ for i = 1 : freq_range_size(2)
   % Phase 2, compute characteristic distances
   dc(i) = a * (k2 * a / 2) ^ (-1 / 3);
   di(i) = (2 * (k1 ^ 2)) / (k2 ^ 3);
+  % Phase 3, compute noise level at this frequency
+  db_noise(i) = fa(log10(FREQ_RANGE(i))) + 10 * log10(BW) - 204 + 30; % dBm
+  % Phase 4, compute attenuation
   for j = 1 : dist_range_size(2)
     d = DIST_RANGE(j);
-    % Phase 3, compute attenuation
     if d > dc % In spherical range
       b(i, j) = nil;
     elseif d < di % In plannar range, near area
@@ -39,15 +49,15 @@ for i = 1 : freq_range_size(2)
                       ((exp(1i * k2 * d)) / (d));
       else % In plannar range, far area
         b(i, j) = ((mu_0 * k1 ^ 2 * current * he) / (2 * pi * k2 ^ 2)) * ...
-                      ((exp(1i * k2 * d)) / (d ^ 2));
-    end % Phase 3
-    % Modelling RX, convert magnetic induction to electrical signal
-    e = abs((-2 * pi * f / k1) * b);
-  end % For i(freq)
-end % For j(dist)
-
-% Convert E to dBm
-db_signal = 20 .* log10(e) + 30;
+                  ((exp(1i * k2 * d)) / (d ^ 2));
+    end
+    % Convert E to dBm
+    e(i, j) = abs((-2 * pi * f / k1) * b(i, j));
+    db_signal(i, j) = 20 .* log10(e(i, j)) + 30;
+    % Compute SNR
+    snr(i, j) = db_signal(i, j) - db_noise(i);
+  end % For j(freq)
+end % For i(dist)
 
 figure;
 hold on;
@@ -55,8 +65,8 @@ plot(FREQ_RANGE, dc);
 plot(FREQ_RANGE, di);
 
 figure;
-mesh(DIST_RANGE, FREQ_RANGE, db_signal);
+mesh(DIST_RANGE, FREQ_RANGE, snr);
 % axis([DIST_RANGE(1) DIST_RANGE(end) FREQ_RANGE(1) FREQ_RANGE(end)]);
 xlabel('Distance(m)');
 ylabel('Frequency(Hz)');
-zlabel('dBm');
+zlabel('SNR(dB)');
